d9a8fc7a31bb18428555795765f3266778830ca5
robbie <robbie@themcnairs.net>
2026-08-13 14:37:15 -0400

    changed website format, added kaggle. Way more complete ipynb file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+// Images
+import scores from './top 5.PNG'
+
 function App() {
   return (
     <div style={{
       fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
-      lineHeight: '1.6',
+      lineHeight: '1.5',
       margin: '0 auto',
-      maxWidth: '800px',
+      maxWidth: '900px',
       padding: '20px',
       color: '#333'
     }}>
@@ -16,29 +19,39 @@ function App() {
       <p>Welcome to my professional portfolio! This is designed to showcase all of my individual work I have done after graduation. This portfolio is a react app made all by me from
         scratch; leveraging Git, GitHub, Firebase, Node.js, Visual Studio Code, Google Colab, and Python.</p>
 
-      <h2 style={{ color: '#2980b9', marginTop: '30px' }}>Featured Work</h2>
-
-      <div style={{ background: '#f9f9f9', border: '1px solid #ddd', padding: '15px', borderRadius: '5px', marginBottom: '20px' }}>
-        <h3>⚽ Kaggle</h3>
-        <p><strong>Tools:</strong> Python, Machine Learning, Data Analysis</p>
-        <p>Machine Learning projects and competitions.</p>
-        <a href="https://www.kaggle.com/rob22bie">Kaggle Profile</a>
-      </div>
-
-
-      <div style={{ background: '#f9f9f9', border: '1px solid #ddd', padding: '15px', borderRadius: '5px', marginBottom: '20px' }}>
-        <h3>⚽ Sports Analytics: World Cup Analysis (WIP) </h3>
-        <p><strong>Tools:</strong> Colab(Python/VS Code), Tableau</p>
-        <p>An exploratory data analysis of event-level data from recent fixtures.</p>
-      </div>
-
       <div style={{ background: '#f9f9f9', border: '1px solid #ddd', padding: '15px', borderRadius: '5px', marginBottom: '20px' }}>
         <h3>My Links</h3>
         <a href="https://github.com/robbiem22/portfolio">View Portfolio on GitHub</a>
+        <p></p>
+        <a href="https://www.kaggle.com/rob22bie">Kaggle Profile</a>
+        <p></p>
         <a href="https://www.linkedin.com/in/robertm22/">LinkedIn</a>
         <p></p>
         <a href="https://jmu.joinhandshake.com/profiles/robbiem">Handshake</a>
       </div>
+
+
+
+      <div style={{ background: '#f9f9f9', border: '1px solid #ddd', padding: '15px', borderRadius: '5px', marginBottom: '20px' }}>
+        <h2>Kaggle</h2>
+        <p><strong>Tools:</strong> Python, Machine Learning, Data Analysis</p>
+        <p>Machine Learning projects and competitions.</p>
+        <h3>Titanic - Machine Learning from Disaster</h3>
+        <p>In this challenge, I built predictive models that answer the question: “what sorts of people
+          were more likely to survive?” using passenger data (ie name, age, gender, socio-economic class, etc).</p>
+        <p>Top 5 Scores(accuracy): </p>
+        <img src={scores} alt="top 5 scores"
+        style={{ width: '100%', height: 'auto', borderRadius: '4px' }}
+        />
+      </div>
+
+
+      <div style={{ background: '#f9f9f9', border: '1px solid #ddd', padding: '15px', borderRadius: '5px', marginBottom: '20px' }}>
+        <h2>⚽ Sports Analytics: World Cup Analysis (WIP) </h2>
+        <p><strong>Tools:</strong> Colab(Python/VS Code), Tableau</p>
+        <p>An exploratory data analysis of event-level data from recent fixtures.</p>
+      </div>
+
     </div>
   );
 }
